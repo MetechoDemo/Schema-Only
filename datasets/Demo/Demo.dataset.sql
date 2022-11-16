9189766e3c3d9a2ce65a2c736ab16c284ba2f52d
jlantz <jasontlantz@gmail.com>
2022-11-16 20:22:34 +0000

    datas

---
+++ datasets/Demo/Demo.dataset.sql
@@ -0,0 +1,9 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"Is_Test__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES(1,'Bluth Company','True');
+COMMIT;
