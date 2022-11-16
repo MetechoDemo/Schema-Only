f08a790ce91f4e937c6f13ddbfe0810889f183d7
jlantz <jasontlantz@gmail.com>
2022-11-16 22:04:15 +0000

    data

--- datasets/Demo/Demo.dataset.sql
+++ datasets/Demo/Demo.dataset.sql
@@ -3,7 +3,8 @@ CREATE TABLE "Account" (
 	id INTEGER NOT NULL, 
 	"Name" VARCHAR(255), 
 	"Is_Test__c" VARCHAR(255), 
+	"Phone" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Account" VALUES(1,'Bluth Company','True');
+INSERT INTO "Account" VALUES(1,'Bluth Company','True','');
 COMMIT;
